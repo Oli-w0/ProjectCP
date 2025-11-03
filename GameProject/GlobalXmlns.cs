c9f8e89b883c0251d6cce3bb9b5e2f7f4d1f79dc
Olek Weglarz <olek@Oleks-MacBook-Pro.local>
2025-11-03 23:07:32 +0000

    Test Commit

---
+++ GameProject/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GameProject")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GameProject.Pages")]
---
+++ GameProject/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GameProject;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        //Position player near bottom center
+        InitializeComponent();
+    }
+}
+        
